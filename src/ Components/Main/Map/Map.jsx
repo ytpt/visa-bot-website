3318df4f2f47ced4f2bedc87e3b3aca14b473017
Annarchive <ytpt@mail.ru>
2022-11-24 14:51:21 +0800

    add the map to Map component and add styles to Map.module.css

---
+++ src/ Components/Main/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Map.module.css';
+
+const Map = () => {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2204.6664907967843!2d84.94914597718129!3d56.45627823758867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4326ecaa7dc9dd13%3A0x37d18bfd09e85b42!2z0L_RgC4g0JvQtdC90LjQvdCwLCAxLCDQotC-0LzRgdC6LCDQotC-0LzRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8sIDYzNDAyOA!5e0!3m2!1sru!2sid!4v1669265171680!5m2!1sru!2sid" allowFullScreen="" loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    )
+}
+
+export default Map;
