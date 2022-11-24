97e282dc3c4c19ca2c0aa81f27a67d8dced00a6b
Annarchive <ytpt@mail.ru>
2022-11-24 15:47:22 +0800

    add route path to Account.jsx in App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from "react";
 import MainPage from "./ Components/Main/MainPage";
 import LoginPage from "./Login/LoginPage";
 import { Routes, Route,  Navigate } from 'react-router-dom';
+import Account from "./ Components/Account/Account";
 
 const App = () => {
-
-
   return (
     <div className='app'>
         <Routes>
             <Route exact path='/' element={<Navigate to='/main'/>} />
             <Route path='/main/' element={<MainPage/>} />
-            <Route path='/login/*' element={<LoginPage/>} />
+            <Route path='/login/*' element={<LoginPage />} />
+            <Route path='/account/*' element={<Account />} />
         </Routes>
      </div>
   );
